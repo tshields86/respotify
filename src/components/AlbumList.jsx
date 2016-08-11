d47c631890519df4836ae694e923dd242b29b9ff
Travis Shields <travis.shields@gmail.com>
2016-08-11 17:37:45 -0400

    Add track preview from spotify api

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
+import Album from './Album';
 
 const AlbumList = (props) => {
-  const albums = props.albums.map((album) => {
-    return (
-      <li>
-        <img
-          src={album.images[1].url}
-          alt={album.name}
-        />
-      </li>
-    );
-  });
+  const albums = props.albums.map((album) =>
+    <Album key={album.id} album={album} getTracks={props.getTracks} />);
 
   return (
-    <ul>
-      {albums}
-    </ul>
+    <div className="col-md-3" style={AlbumList.styles.div}>
+      <ul style={AlbumList.styles.ul}>
+        {albums}
+      </ul>
+    </div>
   );
 };
 
 AlbumList.propTypes = {
   albums: React.PropTypes.array.isRequired,
+  getTracks: React.PropTypes.func.isRequired,
+};
+
+AlbumList.styles = {
+  div: {
+    width: 370,
+    marginLeft: 30,
+    textAlign: 'right',
+    maxHeight: '85vh',
+    overflowY: 'auto',
+  },
+  ul: {
+    listStyle: 'none',
+  },
 };
 
 export default AlbumList;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import AlbumList from './components/AlbumList';
+import TrackList from './components/TrackList';
 import * as musicApi from './api/musicApi';
 
 class App extends React.Component {
@@ -9,26 +10,57 @@ class App extends React.Component {
     super();
     this.state = ({
       albums: [],
+      tracks: [],
+      currentPreview: null,
     });
     this.getAlbums = this.getAlbums.bind(this);
     this.processAlbums = this.processAlbums.bind(this);
+    this.getTracks = this.getTracks.bind(this);
+    this.processTracks = this.processTracks.bind(this);
+    this.playPreview = this.playPreview.bind(this);
   }
 
   getAlbums(artist) {
     musicApi.getAlbums(artist, this.processAlbums);
   }
 
+  getTracks(albumId) {
+    musicApi.getTracks(albumId, this.processTracks);
+  }
+
   processAlbums(payload) {
     this.setState({
       albums: payload.albums.items,
+      tracks: [],
     });
+  }
+
+  processTracks(payload) {
+    this.setState({
+      tracks: payload.tracks.items,
+    });
+  }
+
+  playPreview(previewUrl) {
+    if (this.state.currentPreview) {
+      const curAudioObject = this.state.currentPreview;
+      curAudioObject.pause();
+    }
+
+    const newAudioObject = new Audio(previewUrl);
+    this.setState({
+      currentPreview: newAudioObject,
+    });
+
+    newAudioObject.play();
   }
 
   render() {
     return (
       <div>
         <SearchBar getAlbums={this.getAlbums} />
-        <AlbumList albums={this.state.albums} />
+        <AlbumList albums={this.state.albums} getTracks={this.getTracks} />
+        <TrackList tracks={this.state.tracks} playPreview={this.playPreview} />
       </div>
     );
   }
